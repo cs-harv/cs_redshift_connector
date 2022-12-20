1ee33c90a1f18633d7af2846b5a609b6e81f9a28
cs-harv <115634553+cs-harv@users.noreply.github.com>
2022-12-20 16:55:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup
 setup(name='cs_redshift_connector',
       version='0.1',
       packages=['cs_redshift_connector'],
+      package_data={'cs_redshift_connector': ['*.ini']},
 )
